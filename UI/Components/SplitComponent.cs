7461e46ad7a045e4a5f2932c9accaaeb74898e00
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2021-03-05 17:36:31 -0500

    remove unnecessary variable creation

--- UI/Components/SplitComponent.cs
+++ UI/Components/SplitComponent.cs
@@ -489,7 +489,7 @@ namespace LiveSplit.UI.Components
             if (ColumnsList != null && LabelsList.Count != ColumnsList.Count())
             {
                 LabelsList.Clear();
-                foreach (var column in ColumnsList)
+                for (int i = 0; i < ColumnsList.Count(); i++)
                 {
                     LabelsList.Add(new SimpleLabel
                     {
